afe0876a4ac91894763e897c1b9580426dd4c727
Ben Kang <rkdgusdn9@gmail.com>
2023-06-24 17:58:12 +1000

    Exclude test code and refactor

--- ToDoListDynamic/Program.cs
+++ ToDoListDynamic/Program.cs
@@ -54,34 +54,36 @@ namespace ToDoListDynamic
         private static ToDoItem ChooseType()
         {
             ToDoItem toDoItem = null;
+            var messageList = new List<string>();
+            string wholeMessage;
             Console.WriteLine("\nSelect Type:\n");
             int inputType = Convert.ToInt32(Console.ReadLine());
             switch (inputType)
             {
                 case 1:
                     toDoItem = new Message(inputType);
-                    string test = toDoItem.ToString();
-                    var tests = new List<string>();
-                    tests.Add(test);
-                    foreach(var test1 in tests)
-                    {
-                        Console.WriteLine(test1);
-                    }
+                    wholeMessage = toDoItem.ToString();
+                    messageList.Add(wholeMessage);
+                    //foreach(var test1 in tests)
+                    //{
+                    //    Console.WriteLine(test1);
+                    //}
                     break;
 
                 case 2:
                     toDoItem = new Appointment(inputType);
-                    string test2 = toDoItem.ToString();
-                    var tests1 = new List<string>();
-                    tests1.Add(test2);
-                    foreach (var test3 in tests1)
-                    {
-                        Console.WriteLine(test3);
-                    }
+                    wholeMessage = toDoItem.ToString();
+                    messageList.Add(wholeMessage);
+                    //foreach (var test3 in messageList)
+                    //{
+                    //    Console.WriteLine(test3);
+                    //}
                     break;
 
                 case 3:
                     toDoItem = new Zoom(inputType);
+                    wholeMessage = toDoItem.ToString();
+                    messageList.Add(wholeMessage);
                     break;
             }
 
